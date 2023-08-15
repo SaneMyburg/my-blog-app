5c408890f7056452a47b4319bd3c58b110cde824
SaneMyburg <sane.myburg@gmail.com>
2023-08-15 11:56:48 +0200

    Create migration files and schema

---
+++ db/migrate/20230815092915_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :author_id
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
+
+      t.timestamps
+    end
+
+    add_foreign_key :posts, :users, column: :author_id
+    add_index :posts, :author_id
+  end
+end
