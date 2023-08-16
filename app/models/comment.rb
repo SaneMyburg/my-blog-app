1d66b8669d63a835b3f76f8b02b14975459d09c3
SaneMyburg <sane.myburg@gmail.com>
2023-08-16 09:34:37 +0200

    Add model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :text, presence: true
+
   after_create :update_post_comments_counter
   after_destroy :update_post_comments_counter
 
