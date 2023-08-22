21c6804a34e418bf7629216250ab4335c6fdbc68
SaneMyburg <sane.myburg@gmail.com>
2023-08-22 06:39:27 +0200

    Add requests specs for posts

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  let!(:user) { User.create(name: 'John', posts_counter: 0) }
+  let!(:post) do
+    Post.create(author: user, title: 'My new blog', text: 'This is my first blog', likes_counter: 0,
+                comments_counter: 0)
+  end
+
+  describe 'GET /users/:user_id/posts' do
+    it 'should return a 200 ok status' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render posts/index template' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to render_template('index')
+    end
+
+    it 'should list all posts for correct user' do
+      get user_posts_path(user_id: user.id)
+      expect(response.body).to include('List of all posts and comment of a user')
+    end
+  end
+
+  describe 'GET users/:user_id/posts/:id' do
+    it 'should return a 200 ok status' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render posts/show template' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to render_template('show')
+    end
+
+    it 'should show correct post' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response.body).to include('Details of a selected post')
+    end
+  end
+end
