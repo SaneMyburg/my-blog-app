1ff5d5e8d620b029f3c4659c5fa9312a986c368b
SaneMyburg <sane.myburg@gmail.com>
2023-08-23 07:46:38 +0200

    Create form and method to add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.recent_comments
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_path(@post.author), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
