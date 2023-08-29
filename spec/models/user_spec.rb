1093b5cd17faab20eb3d526c2f305e8b77772a75
SaneMyburg <sane.myburg@gmail.com>
2023-08-29 06:26:33 +0200

    Fix rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe User, type: :model do
 
     it 'returns recent posts in descending order' do
       post1 = Post.create(author: user, title: 'First Post', text: 'Hello', comments_counter: 0, likes_counter: 0,
-                  created_at: 4.days.ago)
+                          created_at: 4.days.ago)
       post2 = Post.create(author: user, title: 'Second Post', text: 'Hello', comments_counter: 0, likes_counter: 0,
                           created_at: 3.days.ago)
       post3 = Post.create(author: user, title: 'Third Post', text: 'Hello', comments_counter: 0, likes_counter: 0,
                           created_at: 2.days.ago)
-      post4 = Post.create(author: user, title: 'Fourth Post', text: 'Hello', comments_counter: 0, likes_counter: 0,
-                          created_at: 1.day.ago)
+      Post.create(author: user, title: 'Fourth Post', text: 'Hello', comments_counter: 0, likes_counter: 0,
+                  created_at: 1.day.ago)
       expect(user.recent_posts).to eq([post1, post2, post3])
     end
   end
