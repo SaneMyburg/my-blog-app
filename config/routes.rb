d7af4f9cbe1a5ca10186780cd27601b9546ad8bd
SaneMyburg <sane.myburg@gmail.com>
2023-09-01 17:43:10 +0200

    Add index route for post controller action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [] do
-        resources :posts, only: [] do
+        resources :posts, only: [:index] do
           resources :comments, only: [:index, :create]
         end
       end
